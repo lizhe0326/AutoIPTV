c73ba774dca4d5a52aca62288430b3995e92feae
lizhe0326 <60695146+lizhe0326@users.noreply.github.com>
2024-09-19 10:16:14 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,15 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-6",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 10
+ipv_type = "all"
