5964ff618a8a581e1434fdbb131c9e857a18ff5c
lizhe0326 <60695146+lizhe0326@users.noreply.github.com>
2024-09-20 10:31:49 +0800

    Create jp_config.py

---
+++ jp_config.py
@@ -0,0 +1,12 @@
+source_file = "jp_demo.txt"
+final_file = "jp_result.txt"
+favorite_list = [
+    "テレビ朝日",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.6
+resolution_weight = 0.4
+recent_days = 30
+ipv_type = "ipv4"
