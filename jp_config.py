a484765ea69820325d99dade7744ee03d76e7795
lizhe0326 <60695146+lizhe0326@users.noreply.github.com>
2024-09-23 11:06:42 +0800

    Create jp_config.py

---
+++ jp_config.py
@@ -0,0 +1,13 @@
+
+source_file = "jp_demo.txt"
+final_file = "jp_result.txt"
+favorite_list = [
+    "广东珠江",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 10
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "all"
